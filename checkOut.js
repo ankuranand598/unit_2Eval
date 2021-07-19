a579af486c55ea9ab1a8296c84b194996b7606b4
ankur598 <ankursingh.as278@gmail.com>
2021-07-19 19:07:21 +0530

    done with card

--- checkOut.js
+++ checkOut.js
@@ -1,4 +1,16 @@
 var div_main=document.getElementById('div_main')
 var p = document.createElement('p');
 p.innerHTML = "Enter card Number";
-div_main.append(p)
+var inp = document.createElement('input');
+inp.type = "number";
+var p2 = document.createElement('p');
+p2.innerHTML = "Enter card Expiry Date";
+var inp2 = document.createElement('input');
+inp2.type = "date";
+var p3 = document.createElement('p');
+p3.innerHTML = "Enter cvv";
+var inp3 = document.createElement('input');
+inp3.type = "number";
+
+
+div_main.append(p,inp,p2,inp2,p3,inp3)
